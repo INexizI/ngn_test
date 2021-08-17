4373abac7e6d26cf998af8d43280524f29476108
INexizI <39536903+INexizI@users.noreply.github.com>
2021-08-18 01:12:02 +0300

    upd TileMap parametrs

--- ngn/code/build.bat
+++ ngn/code/build.bat
@@ -2,12 +2,13 @@
 cls
 
 set CommonCompilerFlags=-MTd -nologo -Gm- -GR- -EHa- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -wd4505 -DMADE_INTERNAL=1 -DMADE_SLOW=1 -DENGINE_WIN32=1 -FC -Z7
-@REM set CommonCompilerFlags=-MT -nologo -Gm- -GR- -EHa- -Od -Oi -W3 -wd4201 -wd4100 -wd4189 -DMADE_INTERNAL=1 -DMADE_SLOW=1 -DENGINE_WIN32=1 -FC -Z7 -Fmwin32_made.map
+REM set CommonCompilerFlags=-MT -nologo -Gm- -GR- -EHa- -Od -Oi -W3 -wd4201 -wd4100 -wd4189 -DMADE_INTERNAL=1 -DMADE_SLOW=1 -DENGINE_WIN32=1 -FC -Z7 -Fmwin32_made.map
 set CommonLinkerFlags= -incremental:no -opt:ref user32.lib gdi32.lib winmm.lib
 
 IF NOT EXIST ..\..\build mkdir ..\..\build
 pushd ..\..\build
 del *.pdb > NUL 2> NUL
+REM Optimization switches /O2 /Oi /fp:fast
 cl %CommonCompilerFlags% ..\ngn\code\engine.cpp -Fmengine.map -LD /link -incremental:no /PDB:engine_%random%.pdb -EXPORT:GameUpdateAndRender -EXPORT:GameGetSoundSamples
 cl %CommonCompilerFlags% ..\ngn\code\win32_made.cpp -Fmwin32_made.map /link %CommonLinkerFlags%
 popd
