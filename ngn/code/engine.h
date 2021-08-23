0a5a8eaa9d85d4176658484d9df390650980653d
INexizI <39536903+INexizI@users.noreply.github.com>
2021-08-24 00:26:26 +0300

    fix Drawing TileMap

--- ngn/code/engine.h
+++ ngn/code/engine.h
@@ -1,113 +1,12 @@
-// #if !defined(ENGINE_H)
-
-// /*
-//     NOTE(D):
-
-//     MADE_INTERNAL:
-//         0 - Build for public release
-//         1 - Build for developer only
-
-//     MADE_SLOW:
-//         0 - Not slow code allowed!
-//         1 - Slow code welcome.
-// */
-
-// #include "engine_platform.h"
-
-// #define internal static
-// #define local_presist static
-// #define global_variable static
-
-// #define Pi32 3.14159265359f
-
-// #if MADE_SLOW
-// #define Assert(Expression) if(!(Expression)) {*(int *)0 = 0;}
-// #else
-// #define Assert(Expression)
-// #endif
-
-// #define Kilobytes(Value) ((Value)*1024LL)
-// #define Megabytes(Value) (Kilobytes(Value)*1024LL)
-// #define Gigabytes(Value) (Megabytes(Value)*1024LL)
-// #define Terabytes(Value) (Gigabytes(Value)*1024LL)
-
-// #define ArrayCount(Array) (sizeof(Array) / sizeof((Array)[0]))
-// // TODO(D): swap, min, max ... macros???
-
-// inline uint32
-// SafeTruncateUInt64(uint64 Value)
-// {
-//     Assert(Value <= 0xFFFFFFFF);
-//     uint32 Result = (uint32)Value;
-//     return (Result);
-// }
-
-// inline game_controller_input *GetController(game_input *Input, int unsigned ControllerIndex)
-// {
-//     Assert(ControllerIndex < ArrayCount(Input->Controllers));
-
-//     game_controller_input *Result = &Input->Controllers[ControllerIndex];
-//     return(Result);
-// }
-
-// //
-// //
-// //
-
-// struct tile_chunk_position
-// {
-//     uint32 TileChunkX;
-//     uint32 TileChunkY;
-
-//     uint32 RelTileX;
-//     uint32 RelTileY;
-// };
-
-// struct world_position
-// {
-//     uint32 AbsTileX;
-//     uint32 AbsTileY;
-
-//     real32 TileRelX;
-//     real32 TileRelY;
-// };
-
-// struct tile_chunk
-// {
-//     uint32 *Tiles;
-// };
-
-// struct world
-// {
-//     uint32 ChunkShift;
-//     uint32 ChunkMask;
-//     uint32 ChunkDim;
-
-//     real32 TileSideInMeters;
-//     int32 TileSideInPixels;
-//     real32 MetersToPixels;
-
-//     int32 TileChunkCountX;
-//     int32 TileChunkCountY;
-
-//     tile_chunk *TileChunks;
-// };
-
-// struct game_state
-// {
-//     world_position PlayerP;
-// };
-
-// #define ENGINE_H
-// #endif
-
 #if !defined(ENGINE_H)
 
 /*
     NOTE(D):
+
     MADE_INTERNAL:
         0 - Build for public release
         1 - Build for developer only
+
     MADE_SLOW:
         0 - Not slow code allowed!
         1 - Slow code welcome.
@@ -122,19 +21,15 @@
 #define Pi32 3.14159265359f
 
 #if MADE_SLOW
-#define Assert(Expression) \
-    if (!(Expression))     \
-    {                      \
-        *(int *)0 = 0;     \
-    }
+#define Assert(Expression) if(!(Expression)) {*(int *)0 = 0;}
 #else
 #define Assert(Expression)
 #endif
 
 #define Kilobytes(Value) ((Value)*1024LL)
-#define Megabytes(Value) (Kilobytes(Value) * 1024LL)
-#define Gigabytes(Value) (Megabytes(Value) * 1024LL)
-#define Terabytes(Value) (Gigabytes(Value) * 1024LL)
+#define Megabytes(Value) (Kilobytes(Value)*1024LL)
+#define Gigabytes(Value) (Megabytes(Value)*1024LL)
+#define Terabytes(Value) (Gigabytes(Value)*1024LL)
 
 #define ArrayCount(Array) (sizeof(Array) / sizeof((Array)[0]))
 // TODO(D): swap, min, max ... macros???
@@ -152,56 +47,55 @@ inline game_controller_input *GetController(game_input *Input, int unsigned Cont
     Assert(ControllerIndex < ArrayCount(Input->Controllers));
 
     game_controller_input *Result = &Input->Controllers[ControllerIndex];
-    return (Result);
+    return(Result);
 }
 
 //
 //
 //
 
-struct canonical_position
+struct tile_chunk_position
 {
-#if 1
-    int32 TileMapX;
-    int32 TileMapY;
+    uint32 TileChunkX;
+    uint32 TileChunkY;
 
-    int32 TileX;
-    int32 TileY;
-#else
-    uint32 _TileX;
-    uint32 _TileY;
-#endif
+    uint32 RelTileX;
+    uint32 RelTileY;
+};
+
+struct world_position
+{
+    uint32 AbsTileX;
+    uint32 AbsTileY;
 
     real32 TileRelX;
     real32 TileRelY;
 };
 
-struct tile_map
+struct tile_chunk
 {
     uint32 *Tiles;
 };
 
 struct world
 {
+    uint32 ChunkShift;
+    uint32 ChunkMask;
+    uint32 ChunkDim;
+
     real32 TileSideInMeters;
     int32 TileSideInPixels;
     real32 MetersToPixels;
 
-    int32 CountX;
-    int32 CountY;
+    int32 TileChunkCountX;
+    int32 TileChunkCountY;
 
-    real32 UpperLeftX;
-    real32 UpperLeftY;
-
-    int32 TileMapCountX;
-    int32 TileMapCountY;
-
-    tile_map *TileMaps;
+    tile_chunk *TileChunks;
 };
 
 struct game_state
 {
-    canonical_position PlayerP;
+    world_position PlayerP;
 };
 
 #define ENGINE_H
